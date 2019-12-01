5b34e0541bb0f44f7a8194bee75ad42c3dae6861
Michele Spinelli <spinellimichele91@protonmail.com>
2019-12-01 21:28:14 +0100

    types exercises folder

---
+++ types/dist/index.js
@@ -0,0 +1,25 @@
+function calculateTax(amount, format) {
+    if (amount === 0) {
+        return null;
+    }
+    const calcAmount = amount * 1.2;
+    return format ? `$${calcAmount.toFixed(2)}` : calcAmount;
+}
+let taxValue = calculateTax(0, false);
+switch (typeof taxValue) {
+    case "number":
+        console.log(`Number Value: ${taxValue.toFixed(2)}`);
+        break;
+    case "string":
+        console.log(`String Value: ${taxValue.charAt(0)}`);
+        break;
+    default:
+        if (taxValue === null) {
+            console.log("Value is null");
+        }
+        else {
+            console.log(typeof taxValue);
+            let value = taxValue;
+            console.log(`Unexpected type for value: ${value}`);
+        }
+}
